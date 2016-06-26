a15ebff3b8ed85a66d86285d5037692bfabbac3e
Vicky Gray <vickygray26@hotmail.com>
2016-06-26 21:57:40 +0100

    Refactored todoAppService into private methods

--- ToDoService.js
+++ ToDoService.js
@@ -1,15 +1,22 @@
 toDoApp.service('ToDoService', ['$http', 'ToDoFactory', function($http, ToDoFactory) {
+  var self = this;
 
-  this.getAll = function() {
+  self.getAll = function() {
     var todos = [];
-    $http.get('http://quiet-beach-24792.herokuapp.com/todos.json')
-      .then(function(response) {
-        response.data.forEach(function(data) {
-          todos.push(new ToDoFactory(data.text, data.completed));
-        });
-      }, function(err) {
-      });
+    _fetchTodosFromApi(todos);
     return todos;
   };
 
+  function _fetchTodosFromApi(todos) {
+    $http.get('http://quiet-beach-24792.herokuapp.com/todos.json')
+      .then(function(response) {
+        _handleResponseFromApi(response.data, todos)
+      }, function(err) {});
+  };
+
+  function _handleResponseFromApi (data, todos) {
+    data.forEach(function(toDoData){
+      todos.push(new ToDoFactory(toDoData.text, toDoData.completed));
+    });
+  }
 }]);
